95b247fd8e48c8d89a579c97c8ccf7ef301a83da
Islam Tashiev <ueaten04@gmail.com>
2023-11-11 11:40:42 +0600

    initial files

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import "./scss/global.scss";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
