fe007983069441dbbc84ca7008dba25f642574b0
Islam Tashiev <ueaten04@gmail.com>
2023-11-17 00:03:41 +0600

    make MainCarouselItem layout

---
+++ src/components/MainCarauselItem.tsx
@@ -0,0 +1,30 @@
+import carouselLogo from "../assets/Carousel-item-logo.png";
+import { ReactComponent as FavoiriteIcon } from "../assets/Favoirite.svg";
+// import carouselBckg from "../assets/Carousel-item-bckg.png";
+
+import "./_style.scss";
+
+interface MainCarauselItemProps {}
+
+const MainCarauselItem = ({}: MainCarauselItemProps) => {
+	return (
+		<div className='main-carousel-item'>
+			<div className='main-carousel-item-content container'>
+				<img className='main-carousel-item-anime-logo' src={carouselLogo} alt='anime-logo' />
+				<p className='main-carousel-item-quote'>
+					It seems like the small leaves in the Village of Konoha have inherited that \"will of
+					fire\" you spoke of, Third. The fire on the leaves will eventually burn greater and
+					stronger... It will flash its light and protect this village.
+				</p>
+				<div className='main-carousel-item-btns'>
+					<button className='btn see-more'>See more</button>
+					<button className='btn btn-icon'>
+						<FavoiriteIcon />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MainCarauselItem;
--- src/pages/Home/components/MainCarousel.tsx
+++ src/pages/Home/components/MainCarousel.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import "./_style.scss";
 
-const MainCarousel = () => {
-	return <div>MainCarousel</div>;
-};
+import { EffectFade, Navigation, Pagination } from "swiper/modules";
+import MainCarauselItem from "../../../components/MainCarauselItem";
 
-export default MainCarousel;
+export default function App() {
+	return (
+		<>
+			<Swiper
+				spaceBetween={30}
+				effect={"fade"}
+				modules={[EffectFade, Navigation, Pagination]}
+				className='mySwiper'
+			>
+				<SwiperSlide>
+					<MainCarauselItem />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MainCarauselItem />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MainCarauselItem />
+				</SwiperSlide>
+			</Swiper>
+		</>
+	);
+}
