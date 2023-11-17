cc58a8d45ce41fa077c68efc3bf68a4bc565e8ee
Islam Tashiev <ueaten04@gmail.com>
2023-11-18 00:26:57 +0600

    make MainCarouse

--- src/components/MainCarauselItem.tsx
+++ src/components/MainCarauselItem.tsx
@@ -1,27 +1,44 @@
-import carouselLogo from "../assets/Carousel-item-logo.png";
-import { ReactComponent as FavoiriteIcon } from "../assets/Favoirite.svg";
+import clsx from "clsx";
+
 import QuoteText from "./QuoteText";
-// import carouselBckg from "../assets/Carousel-item-bckg.png";
+import { ReactComponent as FavoiriteIcon } from "../assets/Favoirite.svg";
 
 import "./_style.scss";
 
-interface MainCarauselItemProps {}
+type info = {
+	logo: string;
+	quote: string;
+	author: string;
+	bckgImage: string;
+};
 
-const MainCarauselItem = ({}: MainCarauselItemProps) => {
+interface MainCarauselItemProps {
+	info: info;
+	activeItem: number;
+	slidesCount: number;
+	slideTo: (slideIndex: number) => void;
+}
+
+const MainCarauselItem = ({ info, activeItem, slidesCount, slideTo }: MainCarauselItemProps) => {
 	return (
-		<div className='main-carousel-item'>
+		<div style={{ background: `url(${info.bckgImage})` }} className='main-carousel-item'>
 			<div className='main-carousel-item-content container'>
-				<img className='main-carousel-item-anime-logo' src={carouselLogo} alt='anime-logo' />
-				<QuoteText
-					author='Jiraya'
-					quote='It seems like the small leaves in the Village of Konoha have inherited that \"will of fire\" you spoke of, Third. The fire on the leaves will eventually burn greater and stronger... It will flash its light and protect this village.'
-					wrapperClass='carousel-quote'
-				/>
+				<img className='main-carousel-item-anime-logo' src={info.logo} alt='anime-logo' />
+				<QuoteText author={info.author} quote={info.quote} wrapperClass='carousel-quote' />
 				<div className='main-carousel-item-btns'>
 					<button className='btn see-more'>See more</button>
 					<button className='btn btn-icon'>
 						<FavoiriteIcon />
 					</button>
+				</div>
+				<div className='pagination'>
+					{new Array(slidesCount).fill(slidesCount).map((_, index) => (
+						<div
+							onClick={() => slideTo(index)}
+							key={Math.random()}
+							className={clsx("pagination-item", { active: activeItem === index })}
+						></div>
+					))}
 				</div>
 			</div>
 		</div>
--- src/pages/Home/components/MainCarousel.tsx
+++ src/pages/Home/components/MainCarousel.tsx
@@ -1,16 +1,52 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import { Swiper as SwiperTypes } from "swiper";
+import { EffectFade, Navigation, Pagination } from "swiper/modules";
+
+import MainCarauselItem from "../../../components/MainCarauselItem";
 
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
 import "./_style.scss";
 
-import { EffectFade, Navigation, Pagination } from "swiper/modules";
-import MainCarauselItem from "../../../components/MainCarauselItem";
+const slides = [
+	{
+		logo: "https://www.pngarts.com/files/11/Ultimate-Ninja-Naruto-Shippuden-Logo-PNG-Picture.png",
+		quote:
+			'It seems like the small leaves in the Village of Konoha have inherited that "will of fire" you spoke of, Third. The fire on the leaves will eventually burn greater and stronger... It will flash its light and protect this village.',
+		author: "Soi Fon",
+		bckgImage:
+			"http://pm1.narvii.com/7216/dad633997999ce8f099da92c13d773e4278f83der1-1500-1000v2_uhq.jpg",
+	},
+	{
+		logo: "https://ww2.freelogovectors.net/wp-content/uploads/2023/01/attackontitanlogo-freelogovectors.net_.png",
+		quote:
+			'It seems like the small leaves in the Village of Konoha have inherited that "will of fire" you spoke of, Third. The fire on the leaves will eventually burn greater and stronger... It will flash its light and protect this village.',
+		author: "Soi Fon",
+		bckgImage:
+			"https://steamuserimages-a.akamaihd.net/ugc/1711916442914027170/FFD57506B1D936A08B9411E9737C9851A85028C6/?imw=512&amp;imh=306&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true",
+	},
+	{
+		logo: "https://i.pinimg.com/originals/49/0b/33/490b33b5b4f103847f062f5d89d03ba5.png",
+		quote:
+			'It seems like the small leaves in the Village of Konoha have inherited that "will of fire" you spoke of, Third. The fire on the leaves will eventually burn greater and stronger... It will flash its light and protect this village.',
+		author: "Soi Fon",
+		bckgImage:
+			"https://wallpapers.com/images/file/luffy-and-one-piece-characters-xg8g2wdde1fl6yhq.jpg",
+	},
+];
 
 export default function App() {
+	const [swiper, setSwiper] = useState<SwiperTypes | null>(null);
+
+	const slideTo = (slideIndex: number) => {
+		if (swiper) {
+			swiper.slideTo(slideIndex);
+		}
+	};
+
 	return (
 		<>
 			<Swiper
@@ -18,16 +54,18 @@ export default function App() {
 				effect={"fade"}
 				modules={[EffectFade, Navigation, Pagination]}
 				className='mySwiper'
+				onInit={(event: SwiperTypes) => setSwiper(event)}
 			>
-				<SwiperSlide>
-					<MainCarauselItem />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MainCarauselItem />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MainCarauselItem />
-				</SwiperSlide>
+				{slides.map((item, index) => (
+					<SwiperSlide key={index}>
+						<MainCarauselItem
+							slideTo={slideTo}
+							info={item}
+							activeItem={swiper?.activeIndex ?? 0}
+							slidesCount={slides.length}
+						/>
+					</SwiperSlide>
+				))}
 			</Swiper>
 		</>
 	);
