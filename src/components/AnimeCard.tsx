d55a2e77691fe44f55c303f1cb9fa202378cd087
Islam Tashiev <ueaten04@gmail.com>
2023-11-21 12:01:25 +0600

    connect layout with db data

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,32 +1,21 @@
 import React from "react";
 import QuoteText from "./QuoteText";
+import { IAnimeCard } from "../models";
 
-const AnimeCard = () => {
+interface AnimeCardProps {
+	info: IAnimeCard;
+}
+
+const AnimeCard = ({ info }: AnimeCardProps) => {
 	return (
 		<div className='anime-card'>
-			<img
-				className='anime-card-image'
-				src='https://i.pinimg.com/originals/80/bf/3b/80bf3bbd22eed39d0c36a30323d94137.jpg'
-				alt='anime-card'
-			/>
+			<img className='anime-card-image' src={info.anime_bckg} alt='anime-card' />
 			<div className='anime-card-content'>
-				<h4 className='anime-card-title'>Attack On Titans</h4>
+				<h4 className='anime-card-title'>{info.anime}</h4>
 				<div className='anime-card-quotes-list'>
-					<QuoteText
-						wrapperClass='anime-card-quote-wrapper'
-						author='Soi Fon'
-						quote='Things like protecting the world are no more than moral reasons that sound good'
-					/>
-					<QuoteText
-						wrapperClass='anime-card-quote-wrapper'
-						author='Soi Fon'
-						quote='Things like protecting the world are no more than moral reasons that sound good'
-					/>
-					<QuoteText
-						wrapperClass='anime-card-quote-wrapper'
-						author='Soi Fon'
-						quote='Things like protecting the world are no more than moral reasons that sound good'
-					/>
+					{info.quotes.map((quote) => (
+						<QuoteText key={quote._id} wrapperClass='anime-card-quote-wrapper' author={quote.character} quote={quote.quote} />
+					))}
 				</div>
 				<button className='btn'>See more</button>
 			</div>
--- src/pages/Home/components/SelectionOfAnimeCarousel.tsx
+++ src/pages/Home/components/SelectionOfAnimeCarousel.tsx
@@ -3,28 +3,22 @@ import AnimeCard from "../../../components/AnimeCard";
 
 import "swiper/css";
 import "./_style.scss";
+import usePagesDataStore from "../../../store/pagesData/pagesData";
+import { useEffect } from "react";
 
 const SelectionOfAnimeCarousel = () => {
+	const { animeCards, getAnimeCards } = usePagesDataStore((state) => state);
+
+	useEffect(() => {
+		getAnimeCards();
+	}, [getAnimeCards]);
 	return (
 		<Swiper className='collection-swiper' slidesPerView={4} spaceBetween={20}>
-			<SwiperSlide>
-				<AnimeCard />
-			</SwiperSlide>
-			<SwiperSlide>
-				<AnimeCard />
-			</SwiperSlide>
-			<SwiperSlide>
-				<AnimeCard />
-			</SwiperSlide>
-			<SwiperSlide>
-				<AnimeCard />
-			</SwiperSlide>
-			<SwiperSlide>
-				<AnimeCard />
-			</SwiperSlide>
-			<SwiperSlide>
-				<AnimeCard />
-			</SwiperSlide>
+			{animeCards.map((card) => (
+				<SwiperSlide key={card._id}>
+					<AnimeCard info={card} />
+				</SwiperSlide>
+			))}
 		</Swiper>
 	);
 };
