48cf8966e709c73ed8d8c74ab65a1c4dce733784
Islam Tashiev <ueaten04@gmail.com>
2023-11-23 12:16:36 +0600

    change collection layout format

--- src/pages/SelectionsDetail/components/QuoteItem.tsx
+++ src/pages/SelectionsDetail/components/QuoteItem.tsx
@@ -2,30 +2,26 @@ import React from "react";
 import { ReactComponent as Reply } from "../../../assets/Reply.svg";
 import { ReactComponent as Save } from "../../../assets/Save.svg";
 import { ReactComponent as Play } from "../../../assets/Play.svg";
-import { IQuote } from "../../../models";
 
 interface QuoteItemProps {
-	info: IQuote;
+	quote: string;
 }
 
-const QuoteItem = ({ info }: QuoteItemProps) => {
+const QuoteItem = ({ quote }: QuoteItemProps) => {
 	return (
 		<div className='quote-item'>
-			<div className='quote-item-header'>
-				<h4 className='quote-item-character'>{info.character}</h4>
-				<div className='quote-item-interface'>
-					<div className='quote-item-interface-item'>
-						<Reply />
-					</div>
-					<div className='quote-item-interface-item'>
-						<Save />
-					</div>
-					<div className='quote-item-interface-item'>
-						<Play />
-					</div>
+			<p className='quote-item-text'>{quote}</p>
+			<div className='quote-item-interface'>
+				<div className='quote-item-interface-item'>
+					<Reply />
+				</div>
+				<div className='quote-item-interface-item'>
+					<Save />
+				</div>
+				<div className='quote-item-interface-item'>
+					<Play />
 				</div>
 			</div>
-			<p className='quote-item-text'>{info.quote}</p>
 		</div>
 	);
 };
--- src/pages/SelectionsDetail/components/QuotesList.tsx
+++ src/pages/SelectionsDetail/components/QuotesList.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import QuoteItem from "./QuoteItem";
 import usePagesDataStore from "../../../store/pagesData/pagesData";
 import QuotesListLoader from "./QuotesListLoader";
+import { IQuote } from "../../../models";
+
+interface IFormatedCollection {
+	quotes: string[];
+	character: string;
+}
 
 const QuotesList = () => {
 	const { collectionDetailData, collectionDetailDataIsLoaded } = usePagesDataStore((state) => state);
+	const [formatedCollection, setFormatedCollection] = useState<IFormatedCollection[]>([]);
+
+	const filterUniqueByField = (arr: IQuote[]) => {
+		const uniqueValuesMap = new Map<string, boolean>();
+		const uniqueValues: string[] = [];
+
+		arr.forEach((item) => {
+			const value = item.character;
+			if (!uniqueValuesMap.has(value)) {
+				uniqueValuesMap.set(value, true);
+				uniqueValues.push(value);
+			}
+		});
+
+		return uniqueValues;
+	};
+
+	useEffect(() => {
+		if (collectionDetailDataIsLoaded) {
+			const filteredQuotes = filterUniqueByField(collectionDetailData[0].quotes).map((name) => {
+				const filteredByCharacter = collectionDetailData[0].quotes.filter((item) => item.character === name).map((item) => item.quote);
+				const mergedQuotes = filteredByCharacter.flat();
+				return { quotes: mergedQuotes, character: name } as IFormatedCollection;
+			});
+			setFormatedCollection(filteredQuotes);
+		}
+	}, [collectionDetailDataIsLoaded, collectionDetailData]);
+
 	return (
 		<div className='quotes-list container'>
 			{collectionDetailDataIsLoaded && (
@@ -13,12 +47,17 @@ const QuotesList = () => {
 				</h2>
 			)}
 
-			{collectionDetailDataIsLoaded ? (
-				<div className='quotes'>
-					{collectionDetailData[0].quotes.map((item) => (
-						<QuoteItem info={item} key={item._id} />
-					))}
-				</div>
+			{collectionDetailDataIsLoaded && formatedCollection.length ? (
+				formatedCollection.map((item) => (
+					<div className='quotes-group'>
+						<h4 className='quotes-group-title'>{item.character}</h4>
+						<div className='quotes'>
+							{item.quotes.map((item) => (
+								<QuoteItem key={item} quote={item} />
+							))}
+						</div>
+					</div>
+				))
 			) : (
 				<QuotesListLoader />
 			)}
