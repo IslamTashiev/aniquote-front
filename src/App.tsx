c2e493dca54120473b3e77da2c2bc3fa2492859e
Islam Tashiev <ueaten04@gmail.com>
2023-11-28 23:59:58 +0600

    add scrollToTop function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Routes from "./Routes";
 
 function App() {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
 	return <Routes />;
 }
 
