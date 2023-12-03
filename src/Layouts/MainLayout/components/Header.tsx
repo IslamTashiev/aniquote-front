fb6d00451e822c19ff11e4dd08397c86c16d8b90
Islam Tashiev <ueaten04@gmail.com>
2023-12-03 19:07:58 +0600

    added navgations to auth pages

--- src/Layouts/MainLayout/components/Header.tsx
+++ src/Layouts/MainLayout/components/Header.tsx
@@ -4,10 +4,12 @@ import clsx from "clsx";
 
 import { ReactComponent as Logo } from "../../../assets/Logo.svg";
 import { ReactComponent as LogoMobile } from "../../../assets/Logo-mobile.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
 	const [mobileMenu, setMobileMenu] = useState<boolean>(false);
+
+	const navigate = useNavigate();
 
 	const handleSwitchMobileMenuState = () => {
 		setMobileMenu(!mobileMenu);
@@ -39,8 +41,12 @@ const Header = () => {
 				</div>
 				<div className='header-interface'>
 					<div className='header-interface-auth'>
-						<button className='btn btn-text header-interface-auth-login'>Login</button>
-						<button className='btn header-interface-auth-signin'>Sign up</button>
+						<button onClick={() => navigate("/auth/login")} className='btn btn-text header-interface-auth-login'>
+							Login
+						</button>
+						<button onClick={() => navigate("/auth/register")} className='btn header-interface-auth-signin'>
+							Sign up
+						</button>
 					</div>
 					<div onClick={handleSwitchMobileMenuState} className={clsx("header-interface-menu", { active: mobileMenu })}>
 						<span></span>
