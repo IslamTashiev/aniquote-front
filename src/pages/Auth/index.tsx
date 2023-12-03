0ce19cccddbb0b1bb4d3bbcff0221279d9e5c358
Islam Tashiev <ueaten04@gmail.com>
2023-12-04 01:18:57 +0600

    change auth page layout

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,21 +1,57 @@
-import React from "react";
-import Register from "./Register";
+import React, { useState } from "react";
+import Input from "../../ui/Input";
 import { ReactComponent as GoogleIcon } from "../../assets/Google.svg";
+import { ReactComponent as HideIcon } from "../../assets/Hide.svg";
+import { ReactComponent as ShowIcon } from "../../assets/Show.svg";
 
 import "./_style.scss";
 import { useParams } from "react-router-dom";
-import Login from "./Login";
 
 const Auth = () => {
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [nickName, setNickName] = useState<string>("");
+	const [showPassword, setShowPassword] = useState<boolean>(false);
+
 	const { authType } = useParams();
+	const isRegisterPage = authType === "register";
+
+	const handleShowPassword = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		setShowPassword(!showPassword);
+	};
+
+	const renderAuthLayout = (
+		<div className='auth'>
+			<h1 className='auth-title'>Register</h1>
+			<form className='auth-form'>
+				<Input label='Email' value={email} onChangeValue={(value) => setEmail(value)} placeholder='example@gmail.com' type='email' isRequired={true} />
+				<div className='auth-password'>
+					<Input
+						inputWrapperStyles='auth-password-input'
+						label='Password'
+						value={password}
+						onChangeValue={(value) => setPassword(value)}
+						placeholder='Enter password'
+						type={showPassword ? "text" : "password"}
+						isRequired={true}
+					/>
+					<button onClick={handleShowPassword} className='btn show-password'>
+						{showPassword ? <HideIcon width={18} height={18} /> : <ShowIcon width={18} height={18} />}
+					</button>
+				</div>
+				{isRegisterPage ? <Input label='Nick Name' value={nickName} onChangeValue={(value) => setNickName(value)} placeholder='Enter your nick name' type='text' isRequired={true} /> : null}
+
+				<button className='btn'>{!isRegisterPage ? "Register" : "Login"}</button>
+			</form>
+		</div>
+	);
 
 	return (
 		<div className='auth-page'>
 			<div className='auth-page-content container'>
 				<div className='auth-page-form'>
-					{authType === "register" && <Register />}
-					{authType === "login" && <Login />}
-
+					{renderAuthLayout}
 					<div className='other-auth-types'>
 						<span>Or join with</span>
 
