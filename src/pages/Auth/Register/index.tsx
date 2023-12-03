a34cacfaf1c90b27bec8258cdb7d2de302bd3249
Islam Tashiev <ueaten04@gmail.com>
2023-12-03 15:05:43 +0600

    added show/hide password functionality

--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../../ui/Input";
+import { ReactComponent as HideIcon } from "../../../assets/Hide.svg";
+import { ReactComponent as ShowIcon } from "../../../assets/Show.svg";
 
 import "./_style.scss";
 
 const Register = () => {
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [nickName, setNickName] = useState<string>("");
+	const [showPassword, setShowPassword] = useState<boolean>(false);
+
+	const handleShowPassword = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		setShowPassword(!showPassword);
+	};
+
 	return (
 		<div className='register'>
 			<h1 className='register-title'>Register</h1>
 			<form className='register-form'>
-				<Input label='Email' onChangeValue={() => {}} placeholder='example@gmail.com' type='email' isRequired={true} />
-				<Input label='Password' onChangeValue={() => {}} placeholder='Enter password' type='password' isRequired={true} />
-				<Input label='Nick Name' onChangeValue={() => {}} placeholder='Enter your nick name' type='text' isRequired={true} />
+				<Input label='Email' value={email} onChangeValue={(value) => setEmail(value)} placeholder='example@gmail.com' type='email' isRequired={true} />
+				<div className='register-password'>
+					<Input
+						inputWrapperStyles='register-password-input'
+						label='Password'
+						value={password}
+						onChangeValue={(value) => setPassword(value)}
+						placeholder='Enter password'
+						type={showPassword ? "text" : "password"}
+						isRequired={true}
+					/>
+					<button onClick={handleShowPassword} className='btn show-password'>
+						{showPassword ? <HideIcon width={18} height={18} /> : <ShowIcon width={18} height={18} />}
+					</button>
+				</div>
+				<Input label='Nick Name' value={nickName} onChangeValue={(value) => setNickName(value)} placeholder='Enter your nick name' type='text' isRequired={true} />
 				<button className='btn'>Submit</button>
 			</form>
 		</div>
