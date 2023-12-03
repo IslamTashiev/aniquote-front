2671ac8e2bffe7fb1ecf4ac27843c5f666731c07
Islam Tashiev <ueaten04@gmail.com>
2023-12-03 19:29:50 +0600

    change loader layot

--- src/pages/SelectionsDetail/components/QuotesListLoader.tsx
+++ src/pages/SelectionsDetail/components/QuotesListLoader.tsx
@@ -3,12 +3,19 @@ import ContentLoader from "react-content-loader";
 
 const QuotesListLoader = () => {
 	return (
-		<ContentLoader speed={2} width={"100%"} height={550} style={{ marginTop: 24 }} backgroundColor='#4b4b4b' foregroundColor='#7d7d7d'>
-			<rect x='0' y='0' rx='10' ry='10' width='100%' height={95} />
-			<rect x='0' y='112' rx='10' ry='10' width='100%' height={95} />
-			<rect x='0' y='224' rx='10' ry='10' width='100%' height={95} />
-			<rect x='0' y='336' rx='10' ry='10' width='100%' height={95} />
-			<rect x='0' y='448' rx='10' ry='10' width='100%' height={95} />
+		<ContentLoader speed={2} width={"100%"} height={850} backgroundColor='#4b4b4b' foregroundColor='#7d7d7d'>
+			<rect x='0' y='0' rx='10' ry='10' width='350' height={24} />
+			<rect x='0' y='48' rx='10' ry='10' width='140' height={24} />
+
+			{[...new Array(5)].map((_, index) => (
+				<rect x='0' y={60 * index + 84 + index * 12} rx='10' ry='10' width='100%' height={60} />
+			))}
+
+			<rect x='0' y='456' rx='10' ry='10' width='140' height={24} />
+
+			{[...new Array(5)].map((_, index) => (
+				<rect x='0' y={60 * index + 492 + index * 12} rx='10' ry='10' width='100%' height={60} />
+			))}
 		</ContentLoader>
 	);
 };
