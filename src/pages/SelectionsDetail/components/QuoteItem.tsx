97eba99d1981025f50381a9925b83734fcb3fda3
Islam Tashiev <ueaten04@gmail.com>
2023-12-04 22:34:38 +0600

    change quote-item layout

--- src/pages/SelectionsDetail/components/QuoteItem.tsx
+++ src/pages/SelectionsDetail/components/QuoteItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { ReactComponent as Reply } from "../../../assets/Reply.svg";
 import { ReactComponent as Save } from "../../../assets/Save.svg";
 import { ReactComponent as Play } from "../../../assets/Play.svg";
@@ -10,13 +10,19 @@ interface QuoteItemProps {
 }
 
 const QuoteItem = ({ quote }: QuoteItemProps) => {
+	const [showComments, setShowComments] = useState<boolean>(false);
+
+	const commentsBlockRef = useRef<HTMLDivElement>(null);
+
+	console.log(commentsBlockRef);
+
 	return (
 		<div className='quote-item'>
 			<div className='quote-item-content'>
 				<p className='quote-item-text'>{quote}</p>
 				<div className='quote-item-interface'>
-					<div className='quote-item-interface-item'>
-						<Reply />
+					<div onClick={() => setShowComments(!showComments)} className='quote-item-interface-item'>
+						<Reply fillOpacity={showComments ? "1" : "0.4"} />
 					</div>
 					<div className='quote-item-interface-item'>
 						<Save />
@@ -26,16 +32,18 @@ const QuoteItem = ({ quote }: QuoteItemProps) => {
 					</div>
 				</div>
 			</div>
-			<div className='quote-item-comments'>
-				<div className='no-comments'>
-					<img className='no-comments-image' src={noComments} alt='no-comments' />
-					<h3 className='no-comments-title'>No Comments Yet</h3>
-					<p className='no-comments-subtitle'>Trailblaze the Discussion: Take the Lead in the Absence of Comments and Add Your Voice</p>
+			<div className='comments-wrapper' style={{ height: showComments ? commentsBlockRef.current?.offsetHeight : 0, marginTop: showComments ? "" : 0 }}>
+				<div ref={commentsBlockRef} className='quote-item-comments'>
+					<div className='no-comments'>
+						<img className='no-comments-image' src={noComments} alt='no-comments' />
+						<h3 className='no-comments-title'>No Comments Yet</h3>
+						<p className='no-comments-subtitle'>Trailblaze the Discussion: Take the Lead in the Absence of Comments and Add Your Voice</p>
+					</div>
+					<form className='comments-form'>
+						<Input inputWrapperStyles='comments-form-input' label='' onChangeValue={() => {}} placeholder='Leave comment' type='text' />
+						<button className='btn'>Leave comment</button>
+					</form>
 				</div>
-				<form className='comments-form'>
-					<Input inputWrapperStyles='comments-form-input' label='' onChangeValue={() => {}} placeholder='Leave comment' type='text' />
-					<button className='btn'>Leave comment</button>
-				</form>
 			</div>
 		</div>
 	);
