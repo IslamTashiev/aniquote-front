00e87a6b51edbee4ac5fb1c370af7c64826ed0a7
Islam Tashiev <ueaten04@gmail.com>
2023-12-04 21:41:36 +0600

    add comments part

--- src/pages/SelectionsDetail/components/QuoteItem.tsx
+++ src/pages/SelectionsDetail/components/QuoteItem.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { ReactComponent as Reply } from "../../../assets/Reply.svg";
 import { ReactComponent as Save } from "../../../assets/Save.svg";
 import { ReactComponent as Play } from "../../../assets/Play.svg";
+import noComments from "../../../assets/NoComments.png";
+import Input from "../../../ui/Input";
 
 interface QuoteItemProps {
 	quote: string;
@@ -10,17 +12,30 @@ interface QuoteItemProps {
 const QuoteItem = ({ quote }: QuoteItemProps) => {
 	return (
 		<div className='quote-item'>
-			<p className='quote-item-text'>{quote}</p>
-			<div className='quote-item-interface'>
-				<div className='quote-item-interface-item'>
-					<Reply />
+			<div className='quote-item-content'>
+				<p className='quote-item-text'>{quote}</p>
+				<div className='quote-item-interface'>
+					<div className='quote-item-interface-item'>
+						<Reply />
+					</div>
+					<div className='quote-item-interface-item'>
+						<Save />
+					</div>
+					<div className='quote-item-interface-item'>
+						<Play />
+					</div>
 				</div>
-				<div className='quote-item-interface-item'>
-					<Save />
+			</div>
+			<div className='quote-item-comments'>
+				<div className='no-comments'>
+					<img className='no-comments-image' src={noComments} alt='no-comments' />
+					<h3 className='no-comments-title'>No Comments Yet</h3>
+					<p className='no-comments-subtitle'>Trailblaze the Discussion: Take the Lead in the Absence of Comments and Add Your Voice</p>
 				</div>
-				<div className='quote-item-interface-item'>
-					<Play />
-				</div>
+				<form className='comments-form'>
+					<Input inputWrapperStyles='comments-form-input' label='' onChangeValue={() => {}} placeholder='Leave comment' type='text' />
+					<button className='btn'>Leave comment</button>
+				</form>
 			</div>
 		</div>
 	);
