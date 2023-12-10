f4bede7949740936fd7ac8d615e6644edc16282f
Islam Tashiev <ueaten04@gmail.com>
2023-12-10 23:03:38 +0600

    done mobile collection page

--- src/pages/Collections/components/CollectionListLoader.tsx
+++ src/pages/Collections/components/CollectionListLoader.tsx
@@ -3,8 +3,8 @@ import ContentLoader from "react-content-loader";
 
 const CollectionListLoader = () => {
 	return (
-		<ContentLoader speed={2} width={"100%"} height={150} backgroundColor='#4b4b4b' foregroundColor='#7d7d7d'>
-			<rect x='0' y='0' rx='10' ry='10' width='100%' height='150' />
+		<ContentLoader className='collection-loader-item' speed={2} width={"100%"} backgroundColor='#4b4b4b' foregroundColor='#7d7d7d'>
+			<rect x='0' y='0' rx='10' ry='10' width='100%' />
 		</ContentLoader>
 	);
 };
