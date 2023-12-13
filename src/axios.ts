6ec6e0151b7227c13a5f38deff0f411388c4cff3
Islam Tashiev <ueaten04@gmail.com>
2023-12-13 23:55:00 +0600

    API_URL

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/";
-const API_URL = "http://localhost:5000/";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/";
 
 const instance = axios.create({
 	baseURL: API_URL,
