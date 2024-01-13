c2838d33b0239a8d9c14e11594e1e1c0d6177913
Islam Tashiev <ueaten04@gmail.com>
2024-01-13 08:56:23 +0600

    add parallax effect to main-poster

--- src/pages/News/components/MainPoster.tsx
+++ src/pages/News/components/MainPoster.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const MainPoster = () => {
 	return (
-		<div className='main-poster'>
-			<div className='main-poster-content container'>
-				<h1 className='main-poster-title'>News</h1>
+		<div className='news-main-poster'>
+			<div className='news-main-poster-content container'>
+				<h1 className='news-main-poster-title'>News</h1>
 				<p>Stay Informed and Inspired with Our Latest Updates</p>
 			</div>
 		</div>
